f5bdf371fe276fc9e7520b1877ad3eeede06bf24
Mainuddin Akash <armainuddinakash1@gmail.com>
2024-03-25 19:11:16 +0600

    Chapter 16: Adding Metadata

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 export default function page() {
   return (
     <div>Customers Page</div>
