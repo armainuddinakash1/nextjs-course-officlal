f9731c7949e942a03fb1b6d5d0db6710355e1bcd
Mainuddin Akash <armainuddinakash1@gmail.com>
2024-04-10 02:28:04 +0600

    Some correction

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,11 +1,26 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
 import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
- 
+
 export const metadata: Metadata = {
   title: 'Customers',
 };
-export default function page() {
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
   return (
-    <div>Customers Page</div>
-  )
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }
